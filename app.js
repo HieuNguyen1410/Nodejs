cb32a95ecf19bb0beb35178270b906d175db688c
HieuNguyen <hieu14101998@gmail.com>
2022-07-19 19:49:05 +0700

    lab 1.4

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const http =  require('http');
 const port = 3000;
 
 const server = http.createServer((req,res) => {
+    const url = req.url;
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<head><head>');
+        res.write('<body><form method="POST" action="/message"><input type="text" name="mes"/><button>Enter</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><head>');
