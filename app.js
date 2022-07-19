0fa0f7699682be5a69474461450ae25bbd504c47
HieuNguyen <hieu14101998@gmail.com>
2022-07-19 20:03:58 +0700

    lab1.5

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const http =  require('http');
+const fs = require('fs');
+
 const port = 3000;
 
 const server = http.createServer((req,res) => {
     const url = req.url;
+    const method = req.method;
     if(url === '/'){
         res.write('<html>');
         res.write('<head><head>');
         res.write('<body><form method="POST" action="/message"><input type="text" name="mes"/><button>Enter</button></form></body>');
         res.write('</html>');
+        return res.end();
+    }
+    if(url ==='/message' && method === 'POST'){
+        fs.writeFile('message.txt','DUMMY',(err)=>{
+            if(err) throw err;
+            console.log('The file has been saved!');
+        });
+        res.statusCode = 302;
+        res.setHeader('location','/');
         return res.end();
     }
     res.setHeader('Content-Type', 'text/html');
