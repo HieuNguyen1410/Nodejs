13ce70373fc674c7119442c822a560f96fbe4463
HieuNguyen <hieu14101998@gmail.com>
2022-07-20 19:18:42 +0700

    lab2.4

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ const app = express();
 
 const routes = require("./routes");
 
-app.use((req, res, next) => {
-  console.log('In the middlewave!');
+app.use('/',(req, res, next) => {
+  console.log('This always run!');
   next();
 });
 
-app.use((req, res, next) => {
+app.use('/add-product',(req, res) => {
+    res.send('<h1>The add product page</h1>')
+})
+
+app.use('/',(req, res) => {
     console.log('In another the middlewave!');
     res.send('Hello from Express.js')
 })
