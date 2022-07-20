29932098aab4f6ff0dfa535bbb028ae3bfe6f5d0
HieuNguyen <hieu14101998@gmail.com>
2022-07-20 20:03:36 +0700

    lab2.9

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,15 @@
 const express = require("express");
 
 const router = express.Router();
-
-router.post("/product", (req, res) => {
+// [GET] /admin/add-product
+router.get("/add-product", (req, res) => {
+  res.send(
+    '<html><head></head><body><form action="/admin/add-product" method="POST"><input type="text" name="title"/><button type="submit">ADD-Product</button></form></body></html>'
+  );
+});
+// [POST] /admin/add-product
+router.post("/add-product", (req, res) => {
   console.log(req.body);
   res.redirect("/");
 });
-
-router.get("/add-product", (req, res) => {
-  res.send(
-    '<html><head></head><body><form action="/product" method="POST"><input type="text" name="title"/><button type="submit">ADD-Product</button></form></body></html>'
-  );
-});
-
 module.exports = router;
