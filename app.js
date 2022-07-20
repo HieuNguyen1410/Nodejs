b9a238c817a4f288b6077c3e5bf4535b73565ec2
HieuNguyen <hieu14101998@gmail.com>
2022-07-20 17:55:18 +0700

    lab1.8

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const routes =  require("./routes");
 
 const port = 3000;
 
-const server = http.createServer(routes);
+const server = http.createServer(routes.requestHandler);
 
 server.listen(port);
