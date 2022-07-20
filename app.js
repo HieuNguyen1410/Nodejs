56815b20a460dab7e59615040195d792e5698c00
HieuNguyen <hieu14101998@gmail.com>
2022-07-20 20:51:08 +0700

    lab2.114

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ const shopRouter = require("./routes/shop");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname,'public')))
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/admin", adminRouter);
 app.use(shopRouter);
 
