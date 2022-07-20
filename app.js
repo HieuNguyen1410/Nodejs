74d438a39ea20df14fbcfe6ac8d95f4bb0648fd6
HieuNguyen <hieu14101998@gmail.com>
2022-07-20 19:56:13 +0700

    lab2.8

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRouter);
 app.use(shopRouter);
 
+app.use((req, res) => {
+    res.status(404).send('<h1>Page not found</h1>')
+})
+
 const port = 3000;
 
 const server = http.createServer(app);
