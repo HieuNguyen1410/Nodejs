5e368420cbc26fcb9b4c1b7c9afd3c931f2228c7
HieuNguyen <hieu14101998@gmail.com>
2022-07-22 18:00:28 +0700

    lab3.7to3.10

--- routes/admin.js
+++ routes/admin.js
@@ -7,11 +7,17 @@ const products = [];
 const router = express.Router();
 // [GET] /admin/add-product
 router.get("/add-product", (req, res) => {
-    res.render('add-product',{pageTitle:'Add Product',path:'/admin/add-product'})
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    formsCSS: true,
+    productCSS:true,
+    activeAddProduct: true,
+  });
 });
 // [POST] /admin/add-product
 router.post("/add-product", (req, res) => {
-  products.push({title: req.body.title})
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 exports.routes = router;
--- routes/shop.js
+++ routes/shop.js
@@ -6,8 +6,15 @@ const adminData = require("./admin");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  const products= adminData.products;
-  res.render('shop',{prods:products,pageTitle:'Shop',path:'/'})
+  const products = adminData.products;
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop:true,
+    productCSS:true,
+  });
 });
 
 module.exports = router;
