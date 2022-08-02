dd5f03f6220b5451ccd24c27fc8ec7e369d2651f
HieuNguyen <hieu14101998@gmail.com>
2022-08-02 19:40:34 +0700

    lab10

--- app.js
+++ app.js
@@ -4,21 +4,44 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 const csrf = require("csurf");
 const flash = require("connect-flash");
-const MongoDBStore = require("connect-mongodb-session")(session);
-const port = 3000;
+const multer = require("multer");
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
 
-const app = express();
+const MONGODB_URI = "mongodb://localhost:27017/shop";
 
+const app = express();
 const store = new MongoDBStore({
-  uri: "mongodb://localhost:27017/shop",
+  uri: MONGODB_URI,
   collection: "sessions",
 });
 const csrfProtection = csrf();
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./images");
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -28,10 +51,14 @@ const shopRoutes = require("./routes/shop");
 const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/images", express.static(path.join(__dirname, "images")));
 app.use(
   session({
-    secret: "My Secret",
+    secret: "my secret",
     resave: false,
     saveUninitialized: false,
     store: store,
@@ -47,6 +74,7 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
+  // throw new Error('Sync Dummy');
   if (!req.session.user) {
     return next();
   }
@@ -67,22 +95,28 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
-app.use("/500", errorController.get500);
+app.get("/500", errorController.get500);
 
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-  res.status(500).render("500", {
-    pageTitle: "Error",
-    path: "/500",
-    isAuthenticated: req.session.isLoggedIn,
-  });
+  // res.status(error.httpStatusCode).render(...);
+  // res.redirect('/500');
+  // res.status(500).render("500", {
+  //   pageTitle: "Error!",
+  //   path: "/500",
+  //   isAuthenticated: req.session.isLoggedIn,
+  // });
+  console.log(error);
 });
 
 mongoose
-  .connect("mongodb://localhost:27017/shop")
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then((result) => {
-    app.listen(port);
+    app.listen(3000);
   })
   .catch((err) => {
     console.log(err);
--- controllers/admin.js
+++ controllers/admin.js
@@ -1,6 +1,9 @@
 const Product = require("../models/product");
 const mongodb = require("mongodb");
 const { validationResult } = require("express-validator/check");
+const fs = require("fs");
+const path = require("path");
+const fileHelper = require("../util/file");
 
 const ObjectId = mongodb.ObjectId;
 
@@ -20,9 +23,26 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
+  const image = req.file;
   const price = req.body.price;
   const description = req.body.description;
+
+  if (!image) {
+    return res.status(422).render("admin/edit-product", {
+      pageTitle: "Add Product",
+      path: "/admin/add-product",
+      editing: false,
+      hasError: true,
+      product: {
+        title: title,
+        price: price,
+        description: description,
+      },
+      errorMessage: "Attached file is not an image.",
+      validationErrors: [],
+    });
+  }
+
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(422).render("admin/edit-product", {
@@ -40,6 +60,9 @@ exports.postAddProduct = (req, res, next) => {
       validationErrors: errors.array(),
     });
   }
+
+  const imageUrl = image.path;
+
   const product = new Product({
     title,
     price,
@@ -106,41 +129,48 @@ exports.getEditProduct = (req, res, next) => {
     });
 };
 
-exports.postEditProduct = (req, res) => {
+exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
   const updatedTitle = req.body.title;
-  const updatedImageUrl = req.body.imageUrl;
   const updatedPrice = req.body.price;
-  const updatedDescription = req.body.description;
+  const image = req.file;
+  const updatedDesc = req.body.description;
+
   const errors = validationResult(req);
+
   if (!errors.isEmpty()) {
     return res.status(422).render("admin/edit-product", {
       pageTitle: "Edit Product",
       path: "/admin/edit-product",
       editing: true,
-      hasError: false,
+      hasError: true,
       product: {
         title: updatedTitle,
-        imageUrl: updatedImageUrl,
         price: updatedPrice,
-        description: updatedDescription,
+        description: updatedDesc,
         _id: prodId,
       },
       errorMessage: errors.array()[0].msg,
       validationErrors: errors.array(),
     });
   }
+
   Product.findById(prodId)
     .then((product) => {
+      if (product.userId.toString() !== req.user._id.toString()) {
+        return res.redirect("/");
+      }
       product.title = updatedTitle;
       product.price = updatedPrice;
-      product.description = updatedDescription;
-      product.imageUrl = updatedImageUrl;
-      return product.save();
-    })
-    .then((result) => {
-      console.log("Updated!!");
-      res.redirect("/admin/products");
+      product.description = updatedDesc;
+      if (image) {
+        fileHelper.deleteFile(product.imageUrl);
+        product.imageUrl = image.path;
+      }
+      return product.save().then((result) => {
+        console.log("UPDATED PRODUCT!");
+        res.redirect("/admin/products");
+      });
     })
     .catch((err) => {
       const error = new Error(err);
@@ -149,11 +179,18 @@ exports.postEditProduct = (req, res) => {
     });
 };
 
-exports.postDeleteProduct = (req, res) => {
+exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByIdAndRemove(prodId)
+  Product.findById(prodId)
+    .then((product) => {
+      if (!product) {
+        return next(new Error("Product not found."));
+      }
+      fileHelper.deleteFile(product.imageUrl);
+      return Product.deleteOne({ _id: prodId, userId: req.user._id });
+    })
     .then(() => {
-      console.log("Product Deleted!");
+      console.log("DESTROYED PRODUCT");
       res.redirect("/admin/products");
     })
     .catch((err) => {
@@ -161,4 +198,16 @@ exports.postDeleteProduct = (req, res) => {
       error.httpStatusCode = 500;
       return next(error);
     });
+
+  // const prodId = req.body.productId;
+  // Product.findByIdAndRemove(prodId)
+  //   .then(() => {
+  //     console.log("Product Deleted!");
+  //     res.redirect("/admin/products");
+  //   })
+  //   .catch((err) => {
+  //     const error = new Error(err);
+  //     error.httpStatusCode = 500;
+  //     return next(error);
+  //   });
 };
