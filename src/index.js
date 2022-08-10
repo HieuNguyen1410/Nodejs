3ff874a1ad7b78ee19d84ce3bd465b6b09c8cc67
HieuNguyen <hieu14101998@gmail.com>
2022-08-10 20:34:46 +0700

    heroku build

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const flash = require("connect-flash");
 const MongoDBStore = require("connect-mongodb-session")(session);
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const ObjectId = mongodb.ObjectId;
 
